de70907b90c3ec8f11b6fafcd78e7c971cabf5e3
ynk-samez <91073921+ynk-samez@users.noreply.github.com>
2022-11-29 01:46:57 +0900

    見た目をいじる部分を分離

--- visualizeNetwork.m
+++ visualizeNetwork.m
@@ -1,24 +1,6 @@
 function visualizeNetwork(Adj)
 
-%全体設定
-INDEX=1:6;
-ROUND=2;%小数点以下何桁で丸めるか
-COLORMAP="cool"; %カラーマップ
-
-%## ノード設定
-NODE_SIZE=8; %ノードのサイズ
-NODE_COLOR="red"; %ノードの色
-
-NODE_NAMES={'SMCL' 'SMCR' 'PMCL' 'PMCR' 'acL' 'acR'};%原点から反時計回りに名前を設定
-
-NODE_LABEL_FONT="Times New Roman"; %ノードラベルのフォント指定
-NODE_LABEL_SIZE=22; %ノードラベルのフォントの大きさ
-NODE_LABEL_COLOR="red";%ノードラベルの文字色
-%## エッジ設定
-EDGE_LABEL_FONT="Times New Roman";%エッジラベルのフォント指定
-EDGE_LABEL_SIZE=12;
-EDGE_LABEL_COLOR="blue";%エッジのラベルの文字色
-
+config;
 %=======以下、基本はいじらない=========
 
 x=1;y=2;
